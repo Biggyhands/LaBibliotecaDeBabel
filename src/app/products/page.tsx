c74191ceb50b570d9f3f43d7b859ea9e98565e79
wsalas19 <willbullet52@gmail.com>
2025-04-08 14:26:26 -0500

    hotfix: Admin issue on register

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import Products from "@/components/products/Products";
-type Props = {};
 
-function page({}: Props) {
-  return <Products />;
+function page() {
+	return <Products />;
 }
 
 export default page;
