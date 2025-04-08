4aa58c2487d2b213c9b045e30a368fdf9704319f
wsalas19 <willbullet52@gmail.com>
2025-04-08 14:15:52 -0500

    refactor: Simplify GET request handling and improve Navbar role management

--- src/app/api/books/[id]/route.ts
+++ src/app/api/books/[id]/route.ts
@@ -1,42 +1,30 @@
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/lib/db";
 
-export async function GET(
-  req: NextRequest,
-  { params }: { params: { id: string } }
-) {
-  const { id } = params;
+export async function GET(req: NextRequest, { params }: { params: Promise<{ id: string }> }) {
+	const { id } = await params;
 
-  try {
-    console.log("Recibido ID para buscar libro:", id); // Log para verificar el ID recibido
+	try {
+		console.log("Recibido ID para buscar libro:", id); // Log para verificar el ID recibido
 
-    if (!id) {
-      console.error("ID no proporcionado en la solicitud.");
-      return NextResponse.json(
-        { error: "ID no proporcionado" },
-        { status: 400 }
-      );
-    }
+		if (!id) {
+			console.error("ID no proporcionado en la solicitud.");
+			return NextResponse.json({ error: "ID no proporcionado" }, { status: 400 });
+		}
 
-    const book = await prisma.books.findUnique({
-      where: { id },
-    });
+		const book = await prisma.books.findUnique({
+			where: { id },
+		});
 
-    if (!book) {
-      console.warn("Libro no encontrado para ID:", id); // Log si no se encuentra el libro
-      return NextResponse.json(
-        { error: "Libro no encontrado" },
-        { status: 404 }
-      );
-    }
+		if (!book) {
+			console.warn("Libro no encontrado para ID:", id); // Log si no se encuentra el libro
+			return NextResponse.json({ error: "Libro no encontrado" }, { status: 404 });
+		}
 
-    console.log("Libro encontrado:", book); // Log para verificar el libro encontrado
-    return NextResponse.json(book);
-  } catch (error) {
-    console.error("Error al obtener el libro:", error);
-    return NextResponse.json(
-      { error: "Error al obtener el libro" },
-      { status: 500 }
-    );
-  }
+		console.log("Libro encontrado:", book); // Log para verificar el libro encontrado
+		return NextResponse.json(book);
+	} catch (error) {
+		console.error("Error al obtener el libro:", error);
+		return NextResponse.json({ error: "Error al obtener el libro" }, { status: 500 });
+	}
 }
